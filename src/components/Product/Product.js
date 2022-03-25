c456ebfebb9af1c31fd8f3ca857d97a643d78580
sadiha-snigdha <nowmi019168@gmail.com>
2022-03-25 22:05:47 +0600

    updated state

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Product.css'
 
-const Product = (props) => {
-    const {img, name, seller, price, ratings} = props.product;
+const Product = ({product, handleAddToCart}) => {
+    // const {product, handleAddToCart} = props;
+    const {img, name, seller, price, ratings} = product;
+    // const {handleAddToCart} = props;
+    
+
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,7 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('products.json')
@@ -11,6 +12,13 @@ const Shop = () => {
         .then(data => setProducts(data))
 
     }, [])
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -18,11 +26,13 @@ const Shop = () => {
                     products.map(product => <Product 
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                         ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Order Summery</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
