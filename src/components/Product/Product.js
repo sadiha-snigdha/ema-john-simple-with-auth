901b888c4fd7d725525d89be2503f79c7ecefad5
sadiha-snigdha <nowmi019168@gmail.com>
2022-03-25 22:55:57 +0600

    new update uploaded

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Product.css'
 
@@ -16,7 +18,11 @@ const Product = ({product, handleAddToCart}) => {
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Ratings: {ratings} stars</small></p>
             </div>
-            <button onClick={() => handleAddToCart(product)} className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+            
         </div>
     );
 };
